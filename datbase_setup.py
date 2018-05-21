b61ddd01d068f849970105d9153c9675cc5172c6
Steven Daut <stmada@gmail.com>
2018-05-21 17:12:12 -0500

    updating database setup script

--- datbase_setup.py
+++ datbase_setup.py
@@ -12,6 +12,22 @@ Base = declarative_base()
 ###insert at end of file ###
 
 engine = create_engine(
-    'sqlite://restaurantmenu.db')
+    'sqlite:///restaurantmenu.db')
 
 Base.metadata.create_all(engine)
+
+class Restaurant(Base):
+    ____tablename__ = 'Restaurant'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    
+
+class MenuItem(Base):
+    __tablename__ = 'MenuItem'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
