a6f0f62ecdfd1fd27f8e425dd776c72a4944909e
Steven Daut <stmada@gmail.com>
2018-05-22 21:02:56 -0500

    updating webserver

---
+++ webserver.py
@@ -0,0 +1,43 @@
+#from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+from http.server import BaseHTTPRequestHandler,HTTPServer
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>Hello!</body></html>"
+                self.wfile.write(output)
+                #print output
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>&#161Hola!<a href = '/hello'>Back to Hello</body></html>"
+                self.wfile.write(output)
+        except:
+            self.send_error(404, "File note Found {}".format(self.path))
+
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print ("Web server running on port %s" % port)
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print ("^C entered, Stopping web server....")    
+        server.socket.close()
+
+
+if __name__ == '__main__':
+    main()
